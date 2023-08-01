1c57541cb4b968be966a28953e757f61c1fc9ca1
taylersala <taylersala@gmail.com>
2023-08-01 12:18:12 -0700

    started

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 // Suggested initial states
 const initialMessage = ''
@@ -10,38 +10,106 @@ export default function AppFunctional(props) {
   // THE FOLLOWING HELPERS ARE JUST RECOMMENDATIONS.
   // You can delete them and build your own logic from scratch.
 
+  const [message, setMessage] = useState(initialMessage);
+  const [email, setEmail] = useState(initialEmail);
+  const [index, setIndex] = useState(initialIndex);
+  const [steps, setSteps] = useState(initialSteps);
+
   function getXY() {
     // It it not necessary to have a state to track the coordinates.
     // It's enough to know what index the "B" is at, to be able to calculate them.
+
+    let x = (index % 3) + 1
+    let y = Math.floor(index / 3) + 1;
+    return { x, y }
   }
 
   function getXYMessage() {
     // It it not necessary to have a state to track the "Coordinates (2, 2)" message for the user.
     // You can use the `getXY` helper above to obtain the coordinates, and then `getXYMessage`
     // returns the fully constructed string.
+    const { x, y } = getXY();
+    return `Coordinates (${x}, ${y})`;
   }
 
   function reset() {
     // Use this helper to reset all states to their initial values.
+    setMessage(initialMessage);
+    setEmail(initialEmail);
+    setSteps(initialSteps);
+    setIndex(initialIndex);
   }
 
   function getNextIndex(direction) {
     // This helper takes a direction ("left", "up", etc) and calculates what the next index
     // of the "B" would be. If the move is impossible because we are at the edge of the grid,
     // this helper should return the current index unchanged.
+    const { index } = this.state;
+    const gridMap = {
+      left: -1,
+      up: -3,
+      right: 1,
+      down: 3,
+    };
+    const nextIndex = index + (gridMap[direction] || 0);
+    console.log("Calculated nextIndex:", nextIndex);
+    return nextIndex >= 0 && nextIndex <= 8 ? nextIndex : index;
   }
 
   function move(evt) {
     // This event handler can use the helper above to obtain a new index for the "B",
     // and change any states accordingly.
+    const direction = evt.target.textContent.toUpperCase();
+    const nextIndex = getNextIndex(direction);
+    setIndex(nextIndex); 
+    setSteps((prevSteps) => prevSteps + 1);
+    setMessage();
+  }
+
+  function updateMessage(){
+    const message = getXYMessage();
+    setMessage(message);
   }
 
   function onChange(evt) {
     // You will need this to update the value of the input.
+    setEmail(evt.target.value);
   }
 
   function onSubmit(evt) {
     // Use a POST request to send a payload to the server.
+    evt.preventDefault();
+
+    const { x, y } = getXY();
+
+    const payload = {
+      x,
+      y,
+      steps,
+      email,
+    };
+
+    fetch('http://localhost:9000/api/result', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log('Server response:', data);
+        reset(); // Reset the state after a successful submission
+      })
+      .catch((error) => {
+        console.error('Error sending POST request:', error);
+      });
+  
   }
 
   return (
