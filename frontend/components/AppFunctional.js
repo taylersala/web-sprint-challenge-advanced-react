1a958866eaf0d02017f580be3c5a0f376e72b30d
taylersala <taylersala@gmail.com>
2023-08-08 12:15:32 -0700

    more working

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -21,15 +21,18 @@ export default function AppFunctional(props) {
     // It's enough to know what index the "B" is at, to be able to calculate them.
 
     let x = (index % 3) + 1
-    let y = Math.floor(index / 3) + 1;
-    return { x, y }
+    let y
+    if (index < 3) y = 1
+    else if (index < 6) y = 2
+    else if (index < 9) y = 3
+    return [ x, y ]
   }
 
   function getXYMessage() {
     // It it not necessary to have a state to track the "Coordinates (2, 2)" message for the user.
     // You can use the `getXY` helper above to obtain the coordinates, and then `getXYMessage`
     // returns the fully constructed string.
-    const { x, y } = getXY();
+    const  [x, y] = getXY();
     return `Coordinates (${x}, ${y})`;
   }
 
@@ -47,39 +50,34 @@ export default function AppFunctional(props) {
 // console.log("Calculated nextIndex:", nextIndex);
   
 
-function getNextIndex(currentIndex, direction) {
-  const gridMap = {
-    left: -1,
-    up: -3,
-    right: 1,
-    down: 3,
-  };
-
-  const nextIndex = currentIndex + (gridMap[direction] || 0);
-
-  console.log("Current index:", index);
-  console.log("Direction:", direction);
-  console.log("Calculated nextIndex:", nextIndex);
-  return nextIndex >= 0 && nextIndex <= 8 ? nextIndex : currentIndex;
+function getNextIndex(direction) {
+  switch (direction) {
+    case 'up':
+      return (index < 3) ? index : index - 3
+    case 'down':
+      return (index > 5) ? index : index + 3
+    case 'left':
+      return (index % 3 === 0) ? index : index - 1
+    case 'right':
+      return ((index - 2) % 3 === 0) ? index : index + 1
+  }
 }
 
-  
-  
-
   function move(evt) {
-    const direction = evt.target.textContent.toUpperCase();
-    const nextIndex = getNextIndex(index, direction); // Pass index directly, not as an object
+    const direction = evt.target.id;
+    const nextIndex = getNextIndex(direction);
   
     // Checking if my next index is within the range
     if (nextIndex !== index) {
       setIndex(nextIndex);
-      setSteps((prevSteps) => prevSteps + 1);
+      setSteps(steps + 1);
       setMessage('');
+      updateMessage();
     } else {
       setMessage(`You can't go ${direction}`);
     }
   
-    updateMessage();
+    // updateMessage();
 }
 
 
